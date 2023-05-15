2c9af80d66d419276e4d4c832330020c97b9b816
thanhd <thanhdanh@icd-vn.com>
2023-05-15 22:20:33 +0700

    test

---
+++ .eslintrc.cjs
@@ -0,0 +1,59 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    // Chúng ta sẽ dùng các rule mặc định từ các plugin mà chúng ta đã cài.
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@typescript-eslint/recommended',
+    // Disable các rule mà eslint xung đột với prettier.
+    // Để cái này ở dưới để nó override các rule phía trên!.
+    'eslint-config-prettier',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh', 'prettier', 'import'],
+  settings: {
+    react: {
+      // Nói eslint-plugin-react tự động biết version của React.
+      version: 'detect'
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
+    // Nói ESLint cách xử lý các import
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      },
+      typescript: {}
+    }
+  },
+  rules: {
+    // Tắt rule yêu cầu import React trong file jsx
+    'react/react-in-jsx-scope': 'off',
+    // Cảnh báo khi thẻ <a target='_blank'> mà không có rel="noreferrer"
+    'react/jsx-no-target-blank': 'warn',
+    'react-refresh/only-export-components': 'warn',
+    // Tăng cường một số rule prettier (copy từ file .prettierrc qua)
+    'prettier/prettier': [
+      'warn',
+      {
+        arrowParens: 'always',
+        semi: false,
+        trailingComma: 'none',
+        tabWidth: 2,
+        endOfLine: 'auto',
+        useTabs: false,
+        singleQuote: true,
+        printWidth: 120,
+        jsxSingleQuote: true
+      }
+    ]
+  }
+}
